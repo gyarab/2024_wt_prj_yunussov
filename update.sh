e22643988737414813aae449f99d4e7acc18c11e
AY-GA <149565612+AY-GA@users.noreply.github.com>
2025-06-23 21:24:48 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+git pull
+
+docker compose exec web python manage.py migrate --noinput
+
+docker compose exec web python manage.py collectstatic --noinput
+
+docker compose exec web touch /app/uwsgi.reload
