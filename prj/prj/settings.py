263c257e65a14fe9535d6d37be78f99122f64760
AY-GA <149565612+AY-GA@users.noreply.github.com>
2025-07-02 15:23:20 +0200

    New info for fixtures

--- prj/prj/settings.py
+++ prj/prj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cwb$t_+hmbq6ie(n8f*z#_p65k*enn_f_n@yhm+_y5k(d+de(=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    
 ]
 
 MIDDLEWARE = [
